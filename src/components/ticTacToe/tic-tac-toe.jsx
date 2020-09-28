6f7a4b71b71437395eface1c5e09df57e01597a6
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-09-27 22:28:26 -0400

    cleaning up the game logic

--- src/components/ticTacToe/tic-tac-toe.jsx
+++ src/components/ticTacToe/tic-tac-toe.jsx
@@ -12,47 +12,50 @@ export default class tictactoe extends Component{
    resetGame(){
       for (let i = 0; i < 3; i++) {
             this.setState(this.state.game[i].fill(''))
-            this.setState({player:'X',win:0})
+            this.setState({player:'X',win:0,winner:''})
          }
       }
    curPlayer(row,box){
-      let winner=this.state.player;
-      if(this.state.game[row][box]!=='X' && this.state.game[row][box]!=='O'){
-         this.state.player==='X'?this.setState({player:'O'}):this.setState({player:'X'})
-         this.setState(this.state.game[row].splice(box,1,this.state.player))
+      if(this.state.win===0){
+         let winner=this.state.player;
+         if(this.state.game[row][box]!=='X' && this.state.game[row][box]!=='O'){
+            this.state.player==='X'?this.setState({player:'O'}):this.setState({player:'X'})
+            this.setState(this.state.game[row].splice(box,1,this.state.player))
+         }
+         this.gameLogic(winner)
       }
-      this.gameLogic(winner)
    }
    gameLogic(w){
          const g=this.state.game;
          const a=g[0];
          const b=g[1];
          const c=g[2];
+         const x='X,X,X';
+         const o='O,O,O';
          let ans=()=>(
          //vertical
-         a[0]==='X' && a[1]===a[0] && a[2]===a[0] ||
-         a[0]==='O' && a[1]===a[0] && a[2]===a[0] ||
-         b[0]==='X' && b[1]===b[0] && b[2]===b[0] ||
-         b[0]==='O' && b[1]===b[0] && b[2]===b[0] ||
-         c[0]==='X' && c[1]===c[0] && c[2]===c[0] ||
-         c[0]==='O' && c[1]===c[0] && c[2]===c[0] ||
+         a.toString()===x ||
+         a.toString()===o ||
+         b.toString()===x ||
+         b.toString()===o ||
+         c.toString()===x ||
+         c.toString()===o ||
          //horizontal
-         a[0]==='X' && b[0]===a[0] && c[0]===a[0] ||
-         a[0]==='O' && b[0]===a[0] && c[0]===a[0] ||
-         a[1]==='X' && b[1]===a[1] && c[1]===a[1] ||
-         a[1]==='O' && b[1]===a[1] && c[1]===a[1] ||
-         a[2]==='X' && b[2]===a[2] && c[2]===a[2] ||
-         a[2]==='O' && b[2]===a[2] && c[2]===a[2] ||
+         [a[0],b[0],c[0]].toString()===x ||
+         [a[0],b[0],c[0]].toString()===o ||
+         [a[1],b[1],c[1]].toString()===x ||
+         [a[1],b[1],c[1]].toString()===o ||
+         [a[2],b[2],c[2]].toString()===x ||
+         [a[2],b[2],c[2]].toString()===o ||
          //diagonal
-         a[0]==='X' && b[1]===a[0] && c[2]===a[0] ||
-         a[0]==='O' && b[1]===a[0] && c[2]===a[0] ||
-         a[2]==='X' && b[1]===a[2] && c[0]===a[2] ||
-         a[2]==='O' && b[1]===a[2] && c[0]===a[2])?this.setState({winner:w,win:1}):
-         (
+         [a[0],b[1],c[2]].toString()===x ||
+         [a[0],b[1],c[2]].toString()===o ||
+         [a[2],b[1],c[0]].toString()===x ||
+         [a[2],b[1],c[0]].toString()===o )?this.setState({winner:w,win:1}):(
          //tie
-         g[0].toString().length===5 &&
-         g[1].toString().length===5 &&
-         g[2].toString().length===5)?this.setState({win:2}):'';
+         a.toString().length===5 &&
+         b.toString().length===5 &&
+         c.toString().length===5)?this.setState({win:2}):'';
          ans()
    }
    createGrid(){
